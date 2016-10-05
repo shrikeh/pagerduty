13d6546a7144309713ad9e93908c1be8a0e5bdf4
Barney Hanlon <shrikeh@gmail.com>
2016-10-05 13:25:21 +0100

    Add example to test usage

--- examples/index.php
+++ examples/index.php
@@ -6,6 +6,9 @@ use Shrikeh\PagerDuty\Application\Provider\Http\Api;
 use Shrikeh\PagerDuty\Application\Provider\Decoder\Json;
 use Shrikeh\PagerDuty\Application\Provider\Auth\Http;
 use Shrikeh\PagerDuty\Application\Provider\Repository\OnCalls\OnCalls;
+use Shrikeh\PagerDuty\Application\Provider\Repository\Users\Users;
+use Shrikeh\PagerDuty\Application\Provider\Hydrator\ContactMethod\Resource;
+use Shrikeh\PagerDuty\Repository\Users\Users as UserRepository;
 
 $container = new Pimple\Container();
 
@@ -13,14 +16,27 @@ $container->register(new Guzzle());
 $container->register(new Json());
 $container->register(new Http());
 $container->register(new Api());
+$container->register(new Resource());
 $container->register(new OnCalls());
+$container->register(new Users());
 
-$container[Api::PROVIDER_HTTP_DOMAIN] = 'api.pagerduty.com';
-$container[Http::PROVIDER_AUTH_TOKEN] = 'w_8PcNuhHa-y3xYdmc1x';
+$container[Api::PROVIDER_HTTP_DOMAIN] = getenv('API_HTTP_DOMAIN');
+$container[Http::PROVIDER_AUTH_TOKEN] = getenv('API_AUTH_TOKEN');
 
-$oncalls = $container[OnCalls::PROVIDER_REPOSITORY_ONCALLS]->get();
+$oncalls = $container[OnCalls::PROVIDER_REPOSITORY_ONCALLS]->users();
 
+foreach ($oncalls->filteredByLevel(1) as $onCall) {
+    $users = $container[Users::PROVIDER_REPOSITORY_USERS]->findById(
+        $onCall->user()->id,
+        array(
+          UserRepository::CONTACT_METHODS,
+          UserRepository::NOTIFICATION_RULES
+        )
+    );
+    foreach ($users as $user) {
+      foreach ($user->contactMethods()->filterByResource('Phone') as $method) {
+          print get_class($method->resource()) . ":$method\n";
+      }
+    }
 
-foreach ($oncalls as $onCall) {
-    var_dump($onCall);
 }
